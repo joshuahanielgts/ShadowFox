0872e3a55189facb7a8ed83a35440b3855b05def
J Joshua Haniel <j06haniel@gmail.com>
2025-05-16 22:53:21 +0530

    Create Variables.py

---
+++ Beginner Level/Variables.py
@@ -0,0 +1,9 @@
+print("Variables Section")
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+P = 10000  
+R = 5      
+T = 3     
+simple_interest = (P * R * T) / 100
+print("Simple Interest for 3 years:", simple_interest)
